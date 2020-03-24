3bd872280902e6b084e757e2db0a92d7d1f5bac0
MilesP <milesparnell@outlook.com>
2020-03-24 15:57:46 -0400

    finished css and js

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,7 +1,25 @@
-$("li").click(function(){
+//adds line through to todos
+$("ul").on("click", "li", function(){
     $(this).toggleClass("done");
 });
-$("span").click(function(e){
-    $(this).parent().fadeOut();
+
+//deletes todos
+$("ul").on("click", "span", function(e){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
     e.stopPropagation();
+});
+
+//to add new todos
+$("input[type='text']").keypress(function(e){
+    if(e.which === 13){
+        let newToDo = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fas fa-trash-alt'></i>   </span> " + newToDo + "</li>");
+    }
+});
+
+$(".fa-minus-square").click(function(){
+    $("input[type='text']").fadeToggle();
 });
